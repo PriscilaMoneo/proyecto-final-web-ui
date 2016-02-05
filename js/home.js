8b71af319d3eb65e4d029bf1849afead6c1e2415
Priscila Moneo <priscila.moneo@globant.com>
2016-02-05 15:00:28 -0300

    order by functionality

--- js/home.js
+++ js/home.js
@@ -23,13 +23,13 @@ function renderizarItems(arrayDeComics) {
       listContent +=   "<img id='img_elm' src='"+ arrayDeComics[i].url +"'/>";
       listContent +=      "<span id='nombre_elm'>"+ arrayDeComics[i].nombre +"</span>";
       listContent +=      "<span id='pto_elm'>Puntaje: "+ arrayDeComics[i].puntaje +"</span>";
-      listContent +=      "<div class='elm_vm'><i class='fa fa-plus'></i> Ver Mas</div>";
+      /*listContent +=      "<div class='elm_vm'><i class='fa fa-plus'></i> Ver Mas</div>";*/
       listContent +=      "</div>";
   }
   return listContent;
 }
 
-function buscar() {
+function buscar(ordenoBusqueda) {
   // El texto a buscar
   var texto_busqueda = document.getElementById('buscarInput').value;
 
@@ -38,17 +38,26 @@ function buscar() {
 
   // Si hay resultados, renderizamos
   if (resultadoBusqueda.length > 0) {
-    resultadoBusqueda = ordenarPor(resultadoBusqueda, campoDeOrden);
+    if (ordenoBusqueda) {
+      resultadoBusqueda = ordenarPor(resultadoBusqueda, campoDeOrden);
+    }
     document.getElementById('content').innerHTML = renderizarItems(resultadoBusqueda);
   } else {
     // Si el texto de busqueda es vacio, entonces mostramos TODOSS los commics de nuevo.
     if (texto_busqueda == '') {
-      resultadoBusqueda = ordenarPor(items.comics, campoDeOrden);
+      if (ordenoBusqueda) {
+        resultadoBusqueda = ordenarPor(items.comics, campoDeOrden);
+      }
       document.getElementById('content').innerHTML = renderizarItems(resultadoBusqueda);
     } else {
       document.getElementById('content').innerHTML = "Sin resultados";
     }
   }
+}
+
+function ordenar(campo) {
+  campoDeOrden = campo;
+  buscar(true); // Llamo a la busqueda pero le digo que Ordene
 }
 
 function filtrarPor(campo, valor) {
@@ -78,11 +87,13 @@ function agregarNuevoComic() {
   var nombre = prompt("Ingrese nombre del Comic", "");
   var personaje = prompt("Ingrese nombre del Personaje", "");
   var puntaje = prompt("Ingrese puntaje", "");
-  var url = prompt("Ingrese URL publica para imagen del comic", "https://upload.wikimedia.org/wikipedia/commons/2/29/WonderworldComics3.jpg");
+  var url = prompt("Ingrese URL publica para imagen del comic", "");
 
   if (nombre != null && personaje != null && puntaje != null) {
 
       items.comics.push({"nombre": nombre, "personaje" : personaje, "url" : url, "puntaje" : puntaje});
+
+      localStorage.setItem("items", JSON.stringify(items));
 
       // Renderizamos de nuevo todos los commiscs
       document.getElementById('content').innerHTML = renderizarItems(items.comics);
@@ -96,21 +107,23 @@ function grabarCampoDeFiltro(campo) {
   campoDeFiltro = campo;
 }
 
-function grabarCampoDeOrden(campo) {
-  campoDeOrden = campo;
-}
+if (localStorage.getItem("items") == null) {
 
-items = {"comics":[
-    {"nombre":"Power Man", "personaje":"Luke Cage","url":"img/comic1.jpg", "puntaje":"2"},
-    {"nombre":"Capitan America", "personaje":"Capitan America","url":"img/comic2.jpg","puntaje":"10"},
-    {"nombre":"X-Men", "personaje":"X-Men","url":"img/comic3.jpg", "puntaje":"3"},
-    {"nombre":"Capitan America", "personaje":"Capitan America - The Falcon","url":"img/comic4.jpg","puntaje":"5"},
-    {"nombre":"Hero for Hire", "personaje":"Luke Cage","url":"img/comic5.jpg", "puntaje":"8"},
-    {"nombre":"The Defenders", "personaje":"The Defenders","url":"img/comic6.jpg","puntaje":"4"},
-    {"nombre":"The Avengers", "personaje":"The Avengers","url":"img/comic7.jpg", "puntaje":"1"},
-    {"nombre":"Hulk", "personaje":"Hulk","url":"img/comic8.jpg","puntaje":"9"},
-    {"nombre":"Patrula X", "personaje":"Patrula X","url":"img/comic9.jpg", "puntaje":"6"},
-    {"nombre":"Silver Surfer", "personaje":"Silver Surfer","url":"img/comic10.jpg","puntaje":"7"},
-]};
+  items = {"comics":[
+      {"nombre":"Power Man", "personaje":"Luke Cage","url":"img/comic1.jpg", "puntaje":"2"},
+      {"nombre":"Capitan America", "personaje":"Capitan America","url":"img/comic2.jpg","puntaje":"10"},
+      {"nombre":"X-Men", "personaje":"X-Men","url":"img/comic3.jpg", "puntaje":"3"},
+      {"nombre":"Capitan America", "personaje":"Capitan America - The Falcon","url":"img/comic4.jpg","puntaje":"5"},
+      {"nombre":"Hero for Hire", "personaje":"Luke Cage","url":"img/comic5.jpg", "puntaje":"8"},
+      {"nombre":"The Defenders", "personaje":"The Defenders","url":"img/comic6.jpg","puntaje":"4"},
+      {"nombre":"The Avengers", "personaje":"The Avengers","url":"img/comic7.jpg", "puntaje":"1"},
+      {"nombre":"Hulk", "personaje":"Hulk","url":"img/comic8.jpg","puntaje":"9"},
+      {"nombre":"Patrula X", "personaje":"Patrula X","url":"img/comic9.jpg", "puntaje":"6"},
+      {"nombre":"Silver Surfer", "personaje":"Silver Surfer","url":"img/comic10.jpg","puntaje":"7"},
+  ]};
+
+} else {
+  items = JSON.parse(localStorage.getItem("items"));
+}
 
 document.getElementById('content').innerHTML = renderizarItems(items.comics);
