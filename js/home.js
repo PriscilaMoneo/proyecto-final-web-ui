13dc5ebfda23cc16bd7fa10b37a679e824f39108
Priscila Moneo <priscila.moneo@globant.com>
2016-02-04 21:16:26 -0300

    items length

--- js/home.js
+++ js/home.js
@@ -11,20 +11,24 @@ function log_out () {
 }
 
 items = {"comics":[
-    {"nombre":"Comic 1", "personaje":"Doe","url":"img/comic1.jpg", "puntaje":"2"},
-    {"nombre":"Comic 2", "personaje":"Peter","url":"img/comic2.jpg","puntaje":"10"},
-    {"nombre":"Comic 3", "personaje":"Doe","url":"img/comic3.jpg", "puntaje":"3"},
-    {"nombre":"Comic 4", "personaje":"Peter","url":"img/comic4.jpg","puntaje":"5"},
-    {"nombre":"Comic 5", "personaje":"Doe","url":"img/comic5.jpg", "puntaje":"8"},
-    {"nombre":"Comic 6", "personaje":"Peter","url":"img/comic6.jpg","puntaje":"4"},
-    {"nombre":"Comic 7", "personaje":"Doe","url":"img/comic7.jpg", "puntaje":"1"},
-    {"nombre":"Comic 8", "personaje":"Peter","url":"img/comic8.jpg","puntaje":"9"},
-    {"nombre":"Comic 9", "personaje":"Doe","url":"img/comic9.jpg", "puntaje":"6"},
-    {"nombre":"Comic 10", "personaje":"Peter","url":"img/comic10.jpg","puntaje":"7"}
+    {"nombre":"Power Man", "personaje":"Luke Cage","url":"img/comic1.jpg", "puntaje":"2"},
+    {"nombre":"Capitan America", "personaje":"Capitan America","url":"img/comic2.jpg","puntaje":"10"},
+    {"nombre":"X-Men", "personaje":"X-Men","url":"img/comic3.jpg", "puntaje":"3"},
+    {"nombre":"Capitan America", "personaje":"Capitan America - The Falcon","url":"img/comic4.jpg","puntaje":"5"},
+    {"nombre":"Hero for Hire", "personaje":"Luke Cage","url":"img/comic5.jpg", "puntaje":"8"},
+    {"nombre":"The Defenders", "personaje":"The Defenders","url":"img/comic6.jpg","puntaje":"4"},
+    {"nombre":"The Avengers", "personaje":"The Avengers","url":"img/comic7.jpg", "puntaje":"1"},
+    {"nombre":"Hulk", "personaje":"Hulk","url":"img/comic8.jpg","puntaje":"9"},
+    {"nombre":"Patrula X", "personaje":"Patrula X","url":"img/comic9.jpg", "puntaje":"6"},
+    {"nombre":"Silver Surfer", "personaje":"Silver Surfer","url":"img/comic10.jpg","puntaje":"7"},
+
 ]}
 
+var tamano = items.comics.length;
+
 var listContent = "";
-for(i=0; i<10; i++){
+
+for(i=0; i<tamano; i++){
     listContent += "<div class='element'>";
     listContent +=   "<img id='img_elm' src='"+ items.comics[i].url +"'/>";
     listContent +=      "<span id='nombre_elm'>"+ items.comics[i].nombre +"</span>";
